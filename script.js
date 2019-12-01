3091418497f0ead45b3361b2af71672c2e00570f
Grigorev Roman <grigorievromka@mail.ru>
2019-12-01 14:43:21 +0300

    Refactoring

--- script.js
+++ script.js
@@ -8,38 +8,10 @@ HERO_MIN_POS_CENTER = (SCREEN_WIDTH - HERO_WIDTH) / 2,
 HERO_MAX_POS_CENTER = BG_WIDTH - SCREEN_WIDTH + HERO_MIN_POS_CENTER;
 
 let
-bg = $('#bg'), // Jq задний фон
 x = 2, // Количество пикселей, которое преодолевает герой при одном нажатии на клавишу
 d = {}, // Состояние клавишь <- и ->
 heroObj = {},
 bgObj = {};
-
-/**
- * Задний фон прокручивается
- */
-function isBgScroll(heroObj)
-{
-	return heroObj.isHeroPosCenter();
-}
-
-/**
- *
- * @param v
- * @param a
- * @param b
- * @param heroObj
- * @returns {number}
- */
-function bgPos(v, a, b, heroObj)
-{
-	let newBgPos = parseInt(v, 10) - (d[a] ? x : 0) + (d[b] ? x : 0),
-	result;
-
-	if (isBgScroll(heroObj)) {
-		result = newBgPos;
-	}
-	return result;
-}
 
 $(window).keydown(function(e) { 
 	d[e.which] = true; 
@@ -50,12 +22,9 @@ $(window).keyup(function(e) {
 });
 
 heroObj = new Hero();
+bgObj = new Bg(heroObj);
 
 setInterval(function() {
 	heroObj.setHeroPos();
-	bg.css({
-		left: function(i, v) {
-			return bgPos(v, 39, 37, heroObj);
-		}
-	});
+	bgObj.setBgPos();
 }, 20);
