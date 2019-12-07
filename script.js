e0e896dfc61ff041e3394f785e2ba95a7d281c74
Grigorev Roman <grigorievromka@mail.ru>
2019-12-07 16:49:18 +0300

    Refactoring.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const
 SCREEN_WIDTH = 1000, // Ширина экрана
-BG_WIDTH = 3000, // Ширина заднего фона
+BG_WIDTH = 2000, // Ширина заднего фона
 HERO_WIDTH = 100, // Ширина героя
 HERO_MIN_POS = 0, // Минимальная позиция героя
 HERO_MAX_POS = BG_WIDTH - HERO_WIDTH, // Максимальная позиция героя
@@ -10,19 +10,59 @@ MONSTER_WIDTH = 100,
 START_MONSTER_FIELD = [SCREEN_WIDTH, BG_WIDTH - MONSTER_WIDTH];
 
 let
-x = 2, // Количество пикселей, которое преодолевает герой при одном нажатии на клавишу
+x = 5, // Количество пикселей, которое преодолевает герой при одном нажатии на клавишу
 d = {}, // Состояние клавишь <- и ->
 heroObj = {}, // Герой
-bgObj = {}, // Задний фон
-monsterPoss = [];
+bgObj = {}; // Задний фон
 
 function getMonsterPoss(startMonsterField)
 {
-	console.log(startMonsterField);
+    let monsterFields = [startMonsterField];
+    let monsterPoss = [];
+    let i = 1;
+    while (i <= 5) {
+        let monsterFieldsNum = monsterFields.length;
+        let currentFieldNum = Math.floor(Math.random() * (monsterFieldsNum - 1 + 1)) + 1;
+        let currentField = monsterFields[currentFieldNum - 1];
+        let monsterPos = Math.floor(Math.random() * (currentField[1] - currentField[0] + 1)) + currentField[0];
+        delete monsterFields[currentFieldNum - 1];
+        let first = [currentField[0], monsterPos];
+        let second = [monsterPos + MONSTER_WIDTH, currentField[1] - MONSTER_WIDTH];
+        monsterFields.push(first);
+        monsterFields.push(second);
+        monsterFields = arrayValues(monsterFields);
+        monsterPoss.push(monsterPos);
+        i++;
+    }
+    return monsterPoss;
+}
+
+function arrayValues(input)
+{
+    let tmp_arr = [],
+    cnt = 0;
+    for (key in input){
+        tmp_arr[cnt] = input[key];
+        cnt++;
+    }
+    return tmp_arr;
+}
+
+function addMonsters(monsterPoss)
+{
+    let monstersNum = monsterPoss.length;
+    let i = 1;
+    while (i <= monstersNum) {
+        bgObj.bg.append('<div class="monster monster-' + i + '"><div>');
+        $('.monster-' + i).css({
+            left: monsterPoss[i - 1]
+        });
+        i++;
+    }
 }
 
 $('#screen').width(SCREEN_WIDTH);
-monsterPoss.push(getMonsterPoss(START_MONSTER_FIELD));
+let monsterPoss = getMonsterPoss(START_MONSTER_FIELD);
 
 $(window).keydown(function(e) { 
 	d[e.which] = true; 
@@ -34,6 +74,8 @@ $(window).keyup(function(e) {
 
 heroObj = new Hero();
 bgObj = new Bg(heroObj);
+
+addMonsters(monsterPoss, bgObj);
 
 setInterval(function() {
 	heroObj.setHeroPos();
