194c16d9b3198acdd5e9b3aa79ed3bb0eca55e05
Grigorev Roman <grigorievromka@mail.ru>
2019-12-07 16:57:21 +0300

    Refactoring.

--- script.js
+++ script.js
@@ -22,16 +22,22 @@ function getMonsterPoss(startMonsterField)
     let i = 1;
     while (i <= 5) {
         let monsterFieldsNum = monsterFields.length;
-        let currentFieldNum = Math.floor(Math.random() * (monsterFieldsNum - 1 + 1)) + 1;
-        let currentField = monsterFields[currentFieldNum - 1];
-        let monsterPos = Math.floor(Math.random() * (currentField[1] - currentField[0] + 1)) + currentField[0];
-        delete monsterFields[currentFieldNum - 1];
-        let first = [currentField[0], monsterPos];
-        let second = [monsterPos + MONSTER_WIDTH, currentField[1] - MONSTER_WIDTH];
-        monsterFields.push(first);
-        monsterFields.push(second);
-        monsterFields = arrayValues(monsterFields);
-        monsterPoss.push(monsterPos);
+        if (monsterFieldsNum > 0) {
+            let currentFieldNum = Math.floor(Math.random() * (monsterFieldsNum - 1 + 1)) + 1;
+            let currentField = monsterFields[currentFieldNum - 1];
+            let monsterPos = Math.floor(Math.random() * (currentField[1] - currentField[0] + 1)) + currentField[0];
+            delete monsterFields[currentFieldNum - 1];
+            let first = [currentField[0], monsterPos - MONSTER_WIDTH];
+            let second = [monsterPos + MONSTER_WIDTH, currentField[1] - MONSTER_WIDTH];
+            if (first[1] - first[0] > MONSTER_WIDTH) {
+                monsterFields.push(first);
+            }
+            if (second[1] - second[0] > MONSTER_WIDTH) {
+                monsterFields.push(second);
+            }
+            monsterFields = arrayValues(monsterFields);
+            monsterPoss.push(monsterPos);
+        }
         i++;
     }
     return monsterPoss;
